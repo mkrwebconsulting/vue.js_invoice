879750058b7bf3a55f5da47a8773cce608bdf94d
mkrwebconsulting <96178355+mkrwebconsulting@users.noreply.github.com>
2022-10-06 15:00:15 +0200

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,14 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBCXPDU9msmZ-o7P4uPaCko5bzyV5LvQbk",
-  authDomain: "invoice-app-75020.firebaseapp.com",
-  projectId: "invoice-app-75020",
-  storageBucket: "invoice-app-75020.appspot.com",
-  messagingSenderId: "916199087916",
-  appId: "1:916199087916:web:433c25001480b7f941d689",
-};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
